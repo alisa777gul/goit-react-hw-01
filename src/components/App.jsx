ce43820221efdde6ee8ca4dd228eeff16e8f5e54
aliso4ka <gulyayevaalisa@gmail.com>
2024-10-04 14:39:48 +0200

    first part

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Profile from './profile/Profile'
+import userData from './profile/userData.json';
+
+function App() {
+ 
+
+  return (
+    <>
+       <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  )
+}
+
+export default App
